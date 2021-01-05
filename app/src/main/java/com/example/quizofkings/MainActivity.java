9c90acb7babf6af6c6c9d40310e44c09c5744837
mobin0263 <mobin0263@gmail.com>
2021-01-05 13:11:59 +0330

    added button action and some fixes made two declared buttons private changed game activity background to "im" changed text size unit from "dp" to "sp" changed background in signup activity from "im" to "back" added large heap= true in app manifest added button intent in main activity which opens other acivities !!!!note: still app crashes when tapin on "کاربر جدید"

--- app/src/main/java/com/example/quizofkings/MainActivity.java
+++ app/src/main/java/com/example/quizofkings/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonSignUp=(Button)findViewById(R.id.btnSignUp);
-        buttonLogin=(Button)findViewById(R.id.btnLogin);
+        buttonSignUp=(Button)findViewById(R.id.newUserButton);
+        buttonLogin=(Button)findViewById(R.id.LoginWelcomeButton);
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
            @Override
             public void onClick(View v) {
